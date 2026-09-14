ee06b914eae5f94116072f8e9888e185485b78cf
easrat <esratjahanafrina189@gmail.com>
2026-09-14 15:35:43 +0600

    header with image

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="q-pa-md">
     <q-layout
-      view="hHh Lpr lff"
+      view="lHh Lpr lff"
       container
       style="height: 300px"
       class="shadow-2 rounded-borders"
     >
-      <q-header
-        elevated
-        :class="$q.dark.isActive ? 'bg-secondary' : 'bg-black'"
-      >
+      <q-header elevated class="bg-cyan-8">
         <q-toolbar>
+          <q-toolbar-title>Header</q-toolbar-title>
           <q-btn
             aria-label="Toggle drawer"
             flat
@@ -19,33 +17,68 @@
             dense
             icon="menu"
           />
-          <q-toolbar-title>Header</q-toolbar-title>
         </q-toolbar>
       </q-header>
 
-      <q-drawer
-        v-model="drawer"
-        show-if-above
-        :width="200"
-        :breakpoint="500"
-        bordered
-        :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
-      >
-        <q-scroll-area class="fit">
-          <q-list role="none">
-            <template v-for="(menuItem, index) in menuList" :key="index">
-              <q-item clickable :active="menuItem.label === 'Inbox'" v-ripple>
-                <q-item-section avatar>
-                  <q-icon :name="menuItem.icon" />
-                </q-item-section>
-                <q-item-section>
-                  {{ menuItem.label }}
-                </q-item-section>
-              </q-item>
-              <q-separator :key="'sep' + index" v-if="menuItem.separator" />
-            </template>
+      <q-drawer v-model="drawer" show-if-above :width="200" :breakpoint="400">
+        <q-scroll-area
+          style="
+            height: calc(100% - 150px);
+            margin-top: 150px;
+            border-right: 1px solid #ddd;
+          "
+        >
+          <q-list padding role="none">
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="inbox" />
+              </q-item-section>
+
+              <q-item-section> Inbox </q-item-section>
+            </q-item>
+
+            <q-item active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="star" />
+              </q-item-section>
+
+              <q-item-section> Star </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="send" />
+              </q-item-section>
+
+              <q-item-section> Send </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="drafts" />
+              </q-item-section>
+
+              <q-item-section> Drafts </q-item-section>
+            </q-item>
           </q-list>
         </q-scroll-area>
+
+        <q-img
+          class="absolute-top"
+          src="https://cdn.quasar.dev/img/material.png"
+          style="height: 150px"
+        >
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img
+                alt="User avatar"
+                src="https://cdn.quasar.dev/img/boy-avatar.png"
+              />
+            </q-avatar>
+            <div class="text-weight-bold">Razvan Stoenescu</div>
+            <div>@rstoenescu</div>
+          </div>
+        </q-img>
       </q-drawer>
 
       <q-page-container>
@@ -64,45 +97,6 @@
 
 <script setup>
 import { ref } from 'vue'
-
-const menuList = [
-  {
-    icon: 'inbox',
-    label: 'Inbox',
-    separator: true
-  },
-  {
-    icon: 'send',
-    label: 'Outbox',
-    separator: false
-  },
-  {
-    icon: 'delete',
-    label: 'Trash',
-    separator: false
-  },
-  {
-    icon: 'error',
-    label: 'Spam',
-    separator: true
-  },
-  {
-    icon: 'settings',
-    label: 'Settings',
-    separator: false
-  },
-  {
-    icon: 'feedback',
-    label: 'Send Feedback',
-    separator: false
-  },
-  {
-    icon: 'help',
-    iconColor: 'primary',
-    label: 'Help',
-    separator: false
-  }
-]
 
 const drawer = ref(false)
 </script>
