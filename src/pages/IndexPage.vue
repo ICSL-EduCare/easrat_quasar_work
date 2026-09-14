94a570e4f2933e300b7077b89baa93c938bc6c11
easrat <esratjahanafrina189@gmail.com>
2026-09-14 15:18:17 +0600

    header with navigation menu

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,110 +1,108 @@
 <template>
-<div class="q-pa-md">
-  <q-layout
-    view="lHh lpr lFf"
-    container
-    style="height: 400px"
-    class="shadow-2 rounded-borders"
-  >
-    <q-header elevated>
-      <q-bar>
-        <q-icon name="laptop_chromebook" />
-        <div>Google Chrome</div>
+  <div class="q-pa-md">
+    <q-layout
+      view="hHh Lpr lff"
+      container
+      style="height: 300px"
+      class="shadow-2 rounded-borders"
+    >
+      <q-header
+        elevated
+        :class="$q.dark.isActive ? 'bg-secondary' : 'bg-black'"
+      >
+        <q-toolbar>
+          <q-btn
+            aria-label="Toggle drawer"
+            flat
+            @click="drawer = !drawer"
+            round
+            dense
+            icon="menu"
+          />
+          <q-toolbar-title>Header</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
 
-        <q-space />
-
-        <q-btn aria-label="Minimize" dense flat icon="minimize" />
-        <q-btn aria-label="Maximize" dense flat icon="crop_square" />
-        <q-btn aria-label="Close" dense flat icon="close" />
-      </q-bar>
-
-      <div class="q-pa-sm q-pl-md row items-center">
-        <div class="cursor-pointer non-selectable">
-          File
-          <q-menu>
-            <q-list dense style="min-width: 100px">
-              <q-item clickable v-close-popup>
-                <q-item-section>Open...</q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup>
-                <q-item-section>New</q-item-section>
-              </q-item>
-
-              <q-separator />
-
-              <q-item clickable>
-                <q-item-section>Preferences</q-item-section>
-                <q-item-section side>
-                  <q-icon name="keyboard_arrow_right" />
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="200"
+        :breakpoint="500"
+        bordered
+        :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+      >
+        <q-scroll-area class="fit">
+          <q-list role="none">
+            <template v-for="(menuItem, index) in menuList" :key="index">
+              <q-item clickable :active="menuItem.label === 'Inbox'" v-ripple>
+                <q-item-section avatar>
+                  <q-icon :name="menuItem.icon" />
                 </q-item-section>
+                <q-item-section>
+                  {{ menuItem.label }}
+                </q-item-section>
+              </q-item>
+              <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+            </template>
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
 
-                <q-menu anchor="top end" self="top start">
-                  <q-list>
-                    <q-item v-for="n in 3" :key="n" dense clickable>
-                      <q-item-section>Submenu Label</q-item-section>
-                      <q-item-section side>
-                        <q-icon name="keyboard_arrow_right" />
-                      </q-item-section>
-                      <q-menu auto-close anchor="top end" self="top start">
-                        <q-list>
-                          <q-item v-for="n in 3" :key="n" dense clickable>
-                            <q-item-section>3rd level Label</q-item-section>
-                          </q-item>
-                        </q-list>
-                      </q-menu>
-                    </q-item>
-                  </q-list>
-                </q-menu>
-              </q-item>
-
-              <q-separator />
-
-              <q-item clickable v-close-popup>
-                <q-item-section>Quit</q-item-section>
-              </q-item>
-            </q-list>
-          </q-menu>
-        </div>
-
-        <div class="q-ml-md cursor-pointer non-selectable">
-          Edit
-          <q-menu auto-close>
-            <q-list dense style="min-width: 100px">
-              <q-item clickable>
-                <q-item-section>Cut</q-item-section>
-              </q-item>
-              <q-item clickable>
-                <q-item-section>Copy</q-item-section>
-              </q-item>
-              <q-item clickable>
-                <q-item-section>Paste</q-item-section>
-              </q-item>
-              <q-separator />
-              <q-item clickable>
-                <q-item-section>Select All</q-item-section>
-              </q-item>
-            </q-list>
-          </q-menu>
-        </div>
-      </div>
-    </q-header>
-
-    <q-page-container>
-      <q-page class="q-pa-md">
-        <p v-for="n in 15" :key="n">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil
-          praesentium molestias a adipisci, dolore vitae odit, quidem
-          consequatur optio voluptates asperiores pariatur eos numquam rerum
-          delectus commodi perferendis voluptate?
-        </p>
-      </q-page>
-    </q-page-container>
-  </q-layout>
-</div>
+      <q-page-container>
+        <q-page padding>
+          <p v-for="n in 15" :key="n">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil
+            praesentium molestias a adipisci, dolore vitae odit, quidem
+            consequatur optio voluptates asperiores pariatur eos numquam rerum
+            delectus commodi perferendis voluptate?
+          </p>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-const tab = ref('images')
+const menuList = [
+  {
+    icon: 'inbox',
+    label: 'Inbox',
+    separator: true
+  },
+  {
+    icon: 'send',
+    label: 'Outbox',
+    separator: false
+  },
+  {
+    icon: 'delete',
+    label: 'Trash',
+    separator: false
+  },
+  {
+    icon: 'error',
+    label: 'Spam',
+    separator: true
+  },
+  {
+    icon: 'settings',
+    label: 'Settings',
+    separator: false
+  },
+  {
+    icon: 'feedback',
+    label: 'Send Feedback',
+    separator: false
+  },
+  {
+    icon: 'help',
+    iconColor: 'primary',
+    label: 'Help',
+    separator: false
+  }
+]
+
+const drawer = ref(false)
 </script>
