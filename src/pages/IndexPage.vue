6610fc15f0300465e4521bd3439444369485538c
easrat <esratjahanafrina189@gmail.com>
2026-09-14 14:27:04 +0600

    desktop view

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,51 +1,106 @@
 <template>
-  <div class="q-pa-md">
-    <q-layout
-      view="lHh lpr lFf"
-      container
-      style="height: 400px"
-      class="shadow-2 rounded-borders"
-    >
-      <q-header bordered class="bg-grey-3 text-primary">
-        <q-toolbar>
-          <q-toolbar-title class="text-center">
-            <q-avatar>
-              <img
-                alt="Quasar logo"
-                src="https://cdn.quasar.dev/logo-v2/svg/logo.svg"
-              />
-            </q-avatar>
-            Quasar Framework
-          </q-toolbar-title>
-        </q-toolbar>
-      </q-header>
+<div class="q-pa-md">
+  <q-layout
+    view="lHh lpr lFf"
+    container
+    style="height: 400px"
+    class="shadow-2 rounded-borders"
+  >
+    <q-header elevated>
+      <q-bar>
+        <q-icon name="laptop_chromebook" />
+        <div>Google Chrome</div>
 
-      <q-footer bordered class="bg-grey-3 text-primary">
-        <q-tabs
-          no-caps
-          active-color="primary"
-          indicator-color="transparent"
-          class="text-grey-8"
-          v-model="tab"
-        >
-          <q-tab name="images" label="Images" />
-          <q-tab name="videos" label="Videos" />
-          <q-tab name="articles" label="Articles" />
-        </q-tabs>
-      </q-footer>
+        <q-space />
 
-      <q-page-container>
-        <q-page class="q-pa-md">
-          <p v-for="n in 15" :key="n">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil
-            praesentium molestias a adipisci, dolore vitae odit, quidem
-            consequatur optio voluptates asperiores pariatur eos numquam rerum
-            delectus commodi perferendis voluptate?
-          </p>
-        </q-page>
-      </q-page-container>
-    </q-layout>
-  </div>
+        <q-btn aria-label="Minimize" dense flat icon="minimize" />
+        <q-btn aria-label="Maximize" dense flat icon="crop_square" />
+        <q-btn aria-label="Close" dense flat icon="close" />
+      </q-bar>
+
+      <div class="q-pa-sm q-pl-md row items-center">
+        <div class="cursor-pointer non-selectable">
+          File
+          <q-menu>
+            <q-list dense style="min-width: 100px">
+              <q-item clickable v-close-popup>
+                <q-item-section>Open...</q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup>
+                <q-item-section>New</q-item-section>
+              </q-item>
+
+              <q-separator />
+
+              <q-item clickable>
+                <q-item-section>Preferences</q-item-section>
+                <q-item-section side>
+                  <q-icon name="keyboard_arrow_right" />
+                </q-item-section>
+
+                <q-menu anchor="top end" self="top start">
+                  <q-list>
+                    <q-item v-for="n in 3" :key="n" dense clickable>
+                      <q-item-section>Submenu Label</q-item-section>
+                      <q-item-section side>
+                        <q-icon name="keyboard_arrow_right" />
+                      </q-item-section>
+                      <q-menu auto-close anchor="top end" self="top start">
+                        <q-list>
+                          <q-item v-for="n in 3" :key="n" dense clickable>
+                            <q-item-section>3rd level Label</q-item-section>
+                          </q-item>
+                        </q-list>
+                      </q-menu>
+                    </q-item>
+                  </q-list>
+                </q-menu>
+              </q-item>
+
+              <q-separator />
+
+              <q-item clickable v-close-popup>
+                <q-item-section>Quit</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </div>
+
+        <div class="q-ml-md cursor-pointer non-selectable">
+          Edit
+          <q-menu auto-close>
+            <q-list dense style="min-width: 100px">
+              <q-item clickable>
+                <q-item-section>Cut</q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section>Copy</q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section>Paste</q-item-section>
+              </q-item>
+              <q-separator />
+              <q-item clickable>
+                <q-item-section>Select All</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </div>
+      </div>
+    </q-header>
+
+    <q-page-container>
+      <q-page class="q-pa-md">
+        <p v-for="n in 15" :key="n">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil
+          praesentium molestias a adipisci, dolore vitae odit, quidem
+          consequatur optio voluptates asperiores pariatur eos numquam rerum
+          delectus commodi perferendis voluptate?
+        </p>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</div>
 </template>
 
 <script setup>
