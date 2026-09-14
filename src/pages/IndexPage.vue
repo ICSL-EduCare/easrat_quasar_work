b578f7982ab23640e5ac3145ef7607f715dd9577
easrat <esratjahanafrina189@gmail.com>
2026-09-14 13:22:53 +0600

    Mixed horizontal and vertical q-gutter

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,32 +1,8 @@
 <template>
   <q-page class="flex flex-center">
-<div class="q-pa-md example-column-horizontal-alignment">
-  <q-badge>items-start</q-badge>
-  <div class="column items-start" style="height: 150px">
-    <div class="col"> One of three rows </div>
-    <div class="col"> One of three rows </div>
-    <div class="col"> One of three rows </div>
-  </div>
-
-  <q-badge>items-center</q-badge>
-  <div class="column items-center" style="height: 150px">
-    <div class="col"> One of three rows </div>
-    <div class="col"> One of three rows </div>
-    <div class="col"> One of three rows </div>
-  </div>
-
-  <q-badge>items-end</q-badge>
-  <div class="column items-end" style="height: 150px">
-    <div class="col"> One of three rows </div>
-    <div class="col"> One of three rows </div>
-    <div class="col"> One of three rows </div>
-  </div>
-
-  <q-badge>self-*</q-badge>
-  <div class="column" style="height: 150px">
-    <div class="col self-start"> .self-start </div>
-    <div class="col self-center"> .self-center </div>
-    <div class="col self-end"> .self-end </div>
+<div class="q-pa-md" style="max-width: 400px">
+  <div class="q-gutter-x-xs q-gutter-y-lg">
+    <q-btn color="purple" label="Button" v-for="n in 10" :key="n" />
   </div>
 </div>
   </q-page>
